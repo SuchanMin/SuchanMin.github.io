3ac555e443d76090b6e3c22b1a0bbbb6f67684be
SuchanMin <suchanwongsakun@gmail.com>
2025-07-17 15:54:37 +0700

    Add files via upload

---
+++ LapSQL01-17.sql
@@ -0,0 +1,5 @@
+select * from Employees
+select EmployeeID , FirstName , LastName From Employees
+select * from Employees where City = 'London'
+select EmployeeID , FirstName, Lastname from Employees where City = 'London'
+select City , Country from Customers
