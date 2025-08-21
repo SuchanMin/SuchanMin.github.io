1b652dc11b7f0af7b8cd7fa6213795f1cc9851d9
SuchanMin <suchanwongsakun@gmail.com>
2025-08-21 13:54:34 +0700

    Add files via upload

--- LapSQL 21.sql
+++ LapSQL 21.sql
@@ -1,3 +1,10 @@
+---แสดงชื่อประเภทสินค้า ชื่อสินค้า และ ราคาสินค้า-------
 Select CategoryName ,ProductName ,UnitPrice 
-From Products, Categories
-Where Products.CategoryID=Categories.CagetgoryID
+From Products as p , Categories as c
+Where P .CategoryID = C.CagetgoryID
+and CategoryName = 'seafood'
+
+
+Select CompanyName = OrderId
+From Orders , Shippers
+Where Shippers.ShipperID = Orders.Shipvia
